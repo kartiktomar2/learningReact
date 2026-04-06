7c861059be66be5532454cfaf0d8439f7062bc26
kartiktomar2 <kartiktomar1717@gmail.com>
2026-04-06 15:15:37 +0530

    added second basic project of context api and dark mode functionality for tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
+// import Card from './components/Card'
 import BgChanger from './components/BgChanger'
 import PasswordGenerator from './components/PasswordGenerator'
 import UseCallbackHook from './components/UseCallbackHook'
@@ -14,43 +13,74 @@ import Calculator from './components/Calculator'
 import UserContextProvider from './components/context/UserContextProvider'
 import Login from './components/context/Login'
 import UserProfile from "./components/context/UserProfile"
+import { ThemeContext } from './components/contextProject2/theme'
+import ThemeBtn from './components/contextProject2/ThemeBtn'
+import Card from './components/contextProject2/Card'
 function App() {
-  let [count, setCount] = useState(0)
+  // let [count, setCount] = useState(0)
+  // function incCount() {
+  //   setCount(count + 1);
+  //   console.log("count ", count)// this will print the value of count from the current render, because react will render the component only after incCount function is completely executed.
+  //   // setCount only queues a state update not triggering the state update imediately.
+
+  // }
+  // function decCount() {
+  //   setCount(count - 1);
+  // }
+
+  const [themeMode, setThemeMode] = useState("light")
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  useEffect(()=>{
+    document.querySelector("html").classList.remove("light","dark")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
   
-  function incCount()
-  {
-       setCount(count+1);
-       console.log("count ", count)// this will print the value of count from the current render, because react will render the component only after incCount function is completely executed.
-       // setCount only queues a state update not triggering the state update imediately.
-
-      }
-
-
-    function decCount()
-    {
-         setCount(count-1);
-    }  
   return (
     <>
-         {/* <div>Count is: {count}</div>
+      {/* <div>Count is: {count}</div>
          <button onClick={incCount}>inc count</button>
          <button onClick={decCount}>dec count</button> */}
-         {/* <Card age={21} name="Kartik" title="developer" arr={[1,2,3]}/>
+      {/* <Card age={21} name="Kartik" title="developer" arr={[1,2,3]}/>
          <Card age={22} name="Kartik Tomar" title="SDE" arr={[4,5,6]}/> */}
-         {/* <BgChanger/> */}
-         {/* <PasswordGenerator/> */}
-         {/* <UseCallbackHook/> */}
-         {/* <Todos/> */}
-         {/* <Currency/> */}
-         {/* <AllRoutes/> */}
-         {/* <StopWatch/> */}
-         {/* <Calculator/> */}
-         
-         {/* First context project */}
-         <UserContextProvider>
+      {/* <BgChanger/> */}
+      {/* <PasswordGenerator/> */}
+      {/* <UseCallbackHook/> */}
+      {/* <Todos/> */}
+      {/* <Currency/> */}
+      {/* <AllRoutes/> */}
+      {/* <StopWatch/> */}
+      {/* <Calculator/> */}
+
+      {/* First context project */}
+      {/* <UserContextProvider>
                <Login/>
                <UserProfile/>
-         </UserContextProvider>
+         </UserContextProvider> */}
+
+
+      {/* Second context project */}
+       <ThemeContext value={{themeMode,darkTheme,lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  {/* theme changing button */}
+                  <ThemeBtn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+                 {/* card component that will response to theme changes  */}
+                 <Card/>
+          </div>
+        </div>
+      </div>
+      </ThemeContext>
+
     </>
   )
 }
